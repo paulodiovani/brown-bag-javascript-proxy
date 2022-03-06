7e8eff8d339f3f1e11c7d0c83940424057223788
Paulo Diovani <paulo@diovani.com>
2022-03-06 18:32:27 -0300

    Replace standard actions with proxy

--- components/counter.tsx
+++ components/counter.tsx
@@ -1,5 +1,5 @@
-import { RootState } from '../reducers';
-import { incrementCount, decrementCount, resetCount } from '../actions'
+import actionCreators from '../action-creators'
+import { RootState } from '../reducers'
 import { useSelector, useDispatch } from 'react-redux'
 
 const Counter = () => {
@@ -11,9 +11,9 @@ const Counter = () => {
       <h1>
         Count: <span>{count}</span>
       </h1>
-      <button onClick={() => dispatch(incrementCount())}>+1</button>
-      <button onClick={() => dispatch(decrementCount())}>-1</button>
-      <button onClick={() => dispatch(resetCount())}>Reset</button>
+      <button onClick={() => dispatch(actionCreators.counterIncrement())}>+1</button>
+      <button onClick={() => dispatch(actionCreators.counterDecrement())}>-1</button>
+      <button onClick={() => dispatch(actionCreators.counterReset())}>Reset</button>
     </div>
   )
 }
